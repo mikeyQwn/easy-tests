2de7e5c4bf670cbfc1c18f1f7d8822666727fd8d
mikeyQwn <mikeyQwn@gmail.com>
2024-03-07 11:59:47 +0300

    Implement answers json upload

--- background.js
+++ background.js
@@ -1,3 +1,49 @@
+/**
+ * @type {Record<string, string>}
+ */
+let answers = {};
+
+/**
+ * @param {unknown} object
+ * @returns {object is {updatedAnswers: Record<string, string>}}
+ */
+function isAnswers(object) {
+    if (!object || typeof object !== "object") {
+        return false;
+    }
+    if (!("updatedAnswers" in object)) {
+        return false;
+    }
+    return Object.values(object.updatedAnswers).every((v) => {
+        return typeof v === "string";
+    });
+}
+
+browser.runtime.onMessage.addListener((req, _sender, sendRes) => {
+    if (!isAnswers(req)) {
+        sendRes({
+            isOk: false,
+            message: "Answers should be\nstring-string json",
+        });
+        return;
+    }
+    browser.tabs.query({ currentWindow: true, active: true }).then((tabs) => {
+        browser.tabs
+            .sendMessage(tabs[0].id, {
+                updatedAnswers: req,
+            })
+            .catch((_) => {
+                console.error(
+                    "[background] Failed to send a message\nto the context script"
+                );
+            });
+    });
+    sendRes({
+        isOk: true,
+        message: "Answers have been\nupdated",
+    });
+});
+
 function sendEvent() {
     browser.tabs.query({ currentWindow: true, active: true }).then((tabs) => {
         browser.tabs
--- popup/index.js
+++ popup/index.js
@@ -1,4 +1,95 @@
-const form = document.getElementById("form");
-if (!form) {
-    console.error("[easy-test error] Your installation is flawed");
+main();
+/**
+ * @typedef {Object} BackgroundAnswer
+ * @property {boolean} isOk
+ * @property {string} message
+ */
+
+/**
+ * @param {unknown} object
+ * @returns {object is BackgroundAnswer}
+ */
+function isBackgroundAnswer(object) {
+    if (!object || typeof object !== "object") {
+        return false;
+    }
+    if (!("isOk" in object && "message" in object)) {
+        return false;
+    }
+    if (typeof object.isOk !== "boolean" || typeof object.message != "string") {
+        return false;
+    }
+    return true;
+}
+
+/**
+ * @param {HTMLDivElement} div
+ * @param {string} message
+ */
+function setError(div, message) {
+    div.classList.remove("success");
+    div.classList.add("error");
+    div.innerText = message;
+}
+
+/**
+ * @param {HTMLDivElement} div
+ * @param {string} message
+ */
+function setSuccess(div, message) {
+    div.classList.remove("error");
+    div.classList.add("success");
+    div.innerText = message;
+}
+
+function main() {
+    /**
+     * @type {HTMLFormElement | null}
+     */
+    const form = document.getElementById("form");
+    /**
+     * @type {HTMLDivElement | null}
+     */
+    const errorMessage = document.getElementById("error-message");
+
+    if (!form || !errorMessage) {
+        console.error("[easy-test error] Your installation is flawed");
+        return;
+    }
+
+    form.onsubmit = (e) => {
+        e.preventDefault();
+        /**
+         * @type {HTMLFormElement}
+         */
+        const form = e.target;
+        const formData = new FormData(form);
+
+        const upload = formData.get("upload");
+        if (!upload) {
+            setError(errorMessage, "Answers field is empty");
+            return;
+        }
+        try {
+            const data = JSON.parse(upload);
+            browser.runtime
+                .sendMessage({ updatedAnswers: data })
+                .then((res) => {
+                    if (!isBackgroundAnswer(res)) {
+                        setError(
+                            errorMessage,
+                            "Cold not get an answer\nfrom background script"
+                        );
+                        return;
+                    }
+                    if (res.isOk) {
+                        setSuccess(errorMessage, res.message);
+                    } else {
+                        setError(errorMessage, res.message);
+                    }
+                });
+        } catch {
+            setError(errorMessage, "The answers are not\nvalid Json");
+        }
+    };
 }
