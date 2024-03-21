503892dd5aa101e0188dbcd4e2530e30097f49bf
mikeyQwn <mikeyQwn@gmail.com>
2024-03-21 19:00:33 +0300

    Add question to answer matching functions

--- background.js
+++ background.js
@@ -19,6 +19,63 @@ function isAnswers(object) {
     });
 }
 
+/**
+ * @param {string} a
+ * @param {string} b
+ * @returns {number}
+ */
+function levDist(a, b) {
+    const m = a.length;
+    const n = b.length;
+
+    let r0 = new Array(n + 1).fill(0).map((_, i) => i);
+    let r1 = new Array(n + 1).fill(0);
+
+    for (let i = 0; i < m; ++i) {
+        r1[0] = i + 1;
+        for (let j = 0; j < n; ++j) {
+            const delCost = r0[j + 1] + 1;
+            const insCost = r1[j] + 1;
+            const subCost = a[i] == b[j] ? r0[j] : r0[j] + 1;
+            r1[j + 1] = Math.min(delCost, insCost, subCost);
+        }
+        [r0, r1] = [r1, r0];
+    }
+
+    return r0[n];
+}
+
+/**
+ * @param {string} a
+ * @param {string} b
+ * @returns {number}
+ */
+function getScore(a, b) {
+    const dist = levDist(a, b);
+    const diff = Math.abs(a.length - b.length);
+    return dist - diff;
+}
+
+/**
+ * @param {Record<string, string>} questionToAnswer
+ * @param {string} question
+ * @param {number} fitness
+ * @returns {number | null}
+ */
+function getAnswer(questionToAnswer, question, fitness) {
+    let minScore = 1_000_000;
+    let ans = null;
+    for (const key of Object.keys(questionToAnswer)) {
+        const score = getScore(key, question);
+        if (score >= minScore) {
+            continue;
+        }
+        minScore = score;
+        ans = questionToAnswer[key];
+    }
+    return minScore <= fitness ? ans : null;
+}
+
 browser.runtime.onMessage.addListener((req, _sender, sendRes) => {
     if (!isAnswers(req)) {
         sendRes({
